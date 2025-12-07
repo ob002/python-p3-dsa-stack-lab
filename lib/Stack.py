fbf2e498ccd7abb0123d0483b45a09fe1ffe221d
Amos Ombworo <amosombworo@gmail.com>
2025-12-07 20:03:43 +0300

    initial

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,34 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        self.items = list(items)
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        for i in range(len(self.items)):
+            if self.items[-(i+1)] == target:
+                return i
+        return -1
